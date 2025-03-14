7cdbf5f549ca2ed8483869272956124b42cedac0
Andrey Garcia Garcia <andreygg300@gmail.com>
2025-03-14 12:53:45 +0100

    commit

--- App/src/main/java/com/repoGAA/organizacion/Organizacion.java
+++ App/src/main/java/com/repoGAA/organizacion/Organizacion.java
@@ -7,4 +7,63 @@ public class Organizacion {
 	String Correo;
 	int Telefono;
 	
+	public Organizacion() {
+	}
+	public Organizacion(String nombre, int numeroPersonas, String correo, int telefono) {
+		this.nombre = nombre;
+		NumeroPersonas = numeroPersonas;
+		Correo = correo;
+		Telefono = telefono;
+	}
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	/**
+	 * @return the numeroPersonas
+	 */
+	public int getNumeroPersonas() {
+		return NumeroPersonas;
+	}
+	/**
+	 * @param numeroPersonas the numeroPersonas to set
+	 */
+	public void setNumeroPersonas(int numeroPersonas) {
+		NumeroPersonas = numeroPersonas;
+	}
+	/**
+	 * @return the correo
+	 */
+	public String getCorreo() {
+		return Correo;
+	}
+	/**
+	 * @param correo the correo to set
+	 */
+	public void setCorreo(String correo) {
+		Correo = correo;
+	}
+	/**
+	 * @return the telefono
+	 */
+	public int getTelefono() {
+		return Telefono;
+	}
+	/**
+	 * @param telefono the telefono to set
+	 */
+	public void setTelefono(int telefono) {
+		Telefono = telefono;
+	}
+	
+	
+	
 }
---
+++ App/src/main/java/com/repoGAA/pago/Pago.java
@@ -0,0 +1,80 @@
+package com.repoGAA.pago;
+
+public class Pago {
+	
+	String Tarjeta;
+	int Efectivo;
+	String Bizum;
+	String paypal;
+	
+	public Pago() {
+	}
+
+	public Pago(String tarjeta, int efectivo, String bizum, String paypal) {
+		Tarjeta = tarjeta;
+		Efectivo = efectivo;
+		Bizum = bizum;
+		this.paypal = paypal;
+	}
+
+	/**
+	 * @return the tarjeta
+	 */
+	public String getTarjeta() {
+		return Tarjeta;
+	}
+
+	/**
+	 * @param tarjeta the tarjeta to set
+	 */
+	public void setTarjeta(String tarjeta) {
+		Tarjeta = tarjeta;
+	}
+
+	/**
+	 * @return the efectivo
+	 */
+	public int getEfectivo() {
+		return Efectivo;
+	}
+
+	/**
+	 * @param efectivo the efectivo to set
+	 */
+	public void setEfectivo(int efectivo) {
+		Efectivo = efectivo;
+	}
+
+	/**
+	 * @return the bizum
+	 */
+	public String getBizum() {
+		return Bizum;
+	}
+
+	/**
+	 * @param bizum the bizum to set
+	 */
+	public void setBizum(String bizum) {
+		Bizum = bizum;
+	}
+
+	/**
+	 * @return the paypal
+	 */
+	public String getPaypal() {
+		return paypal;
+	}
+
+	/**
+	 * @param paypal the paypal to set
+	 */
+	public void setPaypal(String paypal) {
+		this.paypal = paypal;
+	}
+	
+	public void ProcesarPago() {
+		
+	}
+
+}
